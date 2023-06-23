2adccd92cbd96b396603c32412ed0c4f027d043b
AlokhinaTetiana <aliokhinatania03@gmail.com>
2023-06-23 03:07:42 +0300

    f

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -6,10 +6,10 @@ public class NamingConvention {
         int variable_One = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int variable_for = 4;
-        final int variable_five = 5;
-        int variableSix = 6;
+        final int variableFour = 4;
+        final int variableFive = 5;
+        int VariableSix = 6;
         int variableSeven = 7;
-        int variableEight = 8;
+        int VariableEight = 8;
     }
 }
